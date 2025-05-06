4f6f0243f623ecc41dcd71f45e09370849e1e2ba
Ann2009Dol <anny79okfakt.com@gmail.com>
2025-05-06 13:11:14 +0300

    Add files via upload

---
+++ Loader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab3_5.Models
+{
+    public class Loader : ILoader
+    {
+        public event EventHandler Loaded;
+
+        public BolidStatus ReturnCarIntoTheRace()
+        {
+            Crash();
+            return BolidStatus.Normal;
+        }
+
+        protected virtual void Crash()
+        {
+            Loaded?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Mechanic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab3_5.Models
+{
+    public class Mechanic
+    {
+        public BolidStatus changeTires()
+        {
+            return BolidStatus.Normal;
+        }
+    }
+}
+
